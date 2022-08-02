beb07f2fdcc74cc34e689e9587d48d56ddf6d08a
mytoshikaitsolutions <53996653+mytoshikaitsolutions@users.noreply.github.com>
2022-08-02 17:29:23 +0530

    Add files via upload

---
+++ spring-boot-with-logstash-elk-master/src/main/java/com/test/springbootwithelk/SpringbootWithElkApplication.java
@@ -0,0 +1,12 @@
+package com.test.springbootwithelk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithElkApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithElkApplication.class, args);
+	}
+}
